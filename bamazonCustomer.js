f7afc46e64261c2776c2b644231ecbcd674824e7
DanaLittleSkier <dana.alexandrescu@gmail.com>
2019-09-24 23:23:52 -0600

    Created CRUD functions for the Products table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,12 +12,104 @@ var connection = mysql.createConnection({
   
     // Your password
     password: "password1234",
-    database: "top_songsDB"
+    database: "bamazon"
   });
   
   connection.connect(function(err) {
       if (err) throw err;
-      console.log("connected as id " + connection.threadId + "\n");
-      //createProduct();
+      //console.log("connected as id " + connection.threadId + "\n");
+      readProducts();
     });
 
+    function buyerPrompt() {
+        inquirer
+          .prompt([{
+            name: "product_id",
+            type: "input",
+            message: "What is the product id you would like to buy?",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                  return true;
+                }
+                return false;
+              }
+          },
+          {
+            name: "quantity",
+            type: "input",
+            message: "How many units you would like to buy?",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                  return true;
+                }
+                return false;
+              }
+          }
+        ])
+          .then(function(answer) {
+            // based on their answer, either call the bid or the post functions
+            if (answer.product_id > 0) {
+              var query = "SELECT * FROM products where ?";
+              connection.query(query, { item_id: answer.product_id }, function(err, res) { 
+                
+                if(res[0].stock_quantity > answer.quantity){
+                    var newQuantity = res[0].stock_quantity - answer.quantity;
+                    updateProductQuantity(res[0].item_id, newQuantity);
+                    console.log("Purchase complete! Total cost "+ answer.quantity * res[0].price);
+                    readProductsByID(res[0].item_id);
+                    //connection.end();
+                } 
+                else {
+                    console.log("Insufficient quantity!");
+                }      
+            });
+           } 
+            else{
+              console.log("Not a valid id!");
+              connection.end();
+            }
+          });
+      }
+
+    function readProducts() {
+        console.log("Selecting all products...\n");
+        connection.query("SELECT * FROM products", function(err, res) {
+          if (err) throw err;
+          displayItems(res);
+          buyerPrompt();
+        });
+      }
+
+      function readProductsByID(id) {
+        console.log("Selecting product by id...\n");
+        connection.query("SELECT * FROM products where ?", {item_id: id },function(err, res) {
+          if (err) throw err;
+          displayItems(res);
+        });
+      }
+
+      function updateProductQuantity(id, quantity) {
+        console.log("Updating product where id = "+id +"\n");
+
+        connection.query("UPDATE products set ? where ?", 
+        [{
+            stock_quantity: quantity
+        },
+        {
+            item_id: id
+        }],
+        function(err, res,) {
+          if (err) throw err;
+          console.log(res.affectedRows + " products updated!\n"); 
+         
+        });
+        
+      }
+
+      function displayItems(res){
+        res.map(function(element){
+            var line = element.item_id +" || "+element.product_name + " || " +element.department_name + " || "+ element.price +" || " +element.stock_quantity +"\n";
+            console.log(line);
+        });
+      }
+        
